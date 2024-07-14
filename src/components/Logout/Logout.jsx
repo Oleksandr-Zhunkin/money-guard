3da83c35f726000cd8d5b0eebdde76b47c9b93af
LigerCode3443 <161658640+LigerCode3443@users.noreply.github.com>
2024-07-14 15:50:06 +0300

    modal and logout

---
+++ src/components/Logout/Logout.jsx
@@ -0,0 +1,53 @@
+import ModalWindow from "../ModalWindow/ModalWindow";
+import s from "./Logout.module.scss";
+import { useDispatch } from "react-redux";
+import { logoutThunk } from "../../redux/auth/operations";
+import useRespons from "../../hooks/useRespons";
+import { useState } from "react";
+
+const Logout = () => {
+  const dispatch = useDispatch();
+  const { mobileUser } = useRespons();
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+
+  return (
+    <ModalWindow
+      isOpen={modalIsOpen}
+      onClose={() => {
+        setModalIsOpen(false);
+      }}
+    >
+      <div className={s.box}>
+        {!mobileUser && (
+          <>
+            <img
+              src="../public/Money Guard.svg"
+              alt="logo"
+              className={s.logo}
+            />
+            <h4 className={s.title}>Money Guard</h4>
+          </>
+        )}
+
+        <p className={s.text}>Are you sure you want to log out?</p>
+        <button
+          onClick={() => {
+            dispatch(logoutThunk());
+          }}
+          className={s.logout}
+        >
+          logout
+        </button>
+        <button
+          onClick={() => {
+            setModalIsOpen(false);
+          }}
+          className={s.cancel}
+        >
+          cancel
+        </button>
+      </div>
+    </ModalWindow>
+  );
+};
+export default Logout;
