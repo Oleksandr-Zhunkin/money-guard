6dd87465bc7d6cad3af0a9371e23a9e9d727336a
Oleksandr_Zhynkin <godinsighted@gmail.com>
2024-07-17 00:31:27 +0300

    routes, navigation

--- src/components/RestrictedRoute/RestrictedRoute.jsx
+++ src/components/RestrictedRoute/RestrictedRoute.jsx
@@ -1,10 +1,21 @@
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
-const RestrictedRoute = ({ component: Component, redirectTo = "/" }) => {
-  const isLogedIn = useSelector(selectIsLoggedIn);
+// const RestrictedRoute = ({ component: Component, redirectTo = "/" }) => {
+//   const isLogedIn = useSelector(selectIsLoggedIn);
+//   console.log(location);
 
-  return isLogedIn ? <Navigate to={redirectTo} /> : Component;
+//   return isLogedIn ? <Navigate to={redirectTo} /> : Component;
+// };
+//
+const RestrictedRoute = ({ children }) => {
+  const location = useLocation();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  return isLoggedIn ? (
+    <Navigate to={location?.state || "/"} replace />
+  ) : (
+    children
+  );
 };
 export default RestrictedRoute;
