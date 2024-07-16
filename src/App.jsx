888d820aba330bb82d4d55aca502f8a17fd09cbf
Oleksandr_Zhynkin <godinsighted@gmail.com>
2024-07-17 00:52:26 +0300

    fix currency

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Section from "./components/Section/Section";
 import Layout from "./components/Layout/Layout";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
-
 import { LoginPage } from "./pages/LoginPage/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage/RegisterPage";
 import HomePage from "./pages/HomePage/HomePage";
@@ -14,18 +13,18 @@ import Loader from "./components/Loader/Loader";
 import { refreshThunk } from "./redux/auth/operations";
 import { selectIsRefresh } from "./redux/auth/selectors";
 import Currency from "./components/Currency/Currency.jsx";
-
 import StatisticDatePicker from "./components/StatisticsDataPicker/StatisticsDatePicker.jsx";
-
 import Statistics from "./pages/Statistics/Statistics.jsx";
 import {
   categoriesThunk,
   summaryThunk,
 } from "./redux/categories/operations.js";
+import useRespons from "./hooks/useRespons.js";
 
 function App() {
   const dispatch = useDispatch();
   const isRefreshing = useSelector(selectIsRefresh);
+  const { mobileUser } = useRespons();
 
   useEffect(() => {
     dispatch(refreshThunk());
@@ -49,8 +48,8 @@ function App() {
               }
             >
               <Route index element={<HomePage />} />
-              <Route path="currency" element={<Currency />} />
               <Route path="statistics" element={<Statistics />} />
+              <Route path="currency" element={mobileUser && <Currency />} />
             </Route>
             <Route
               path="/login"
